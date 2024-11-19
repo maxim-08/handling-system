cdd56ce22564cd9af49eec6eaa80d92c22fc118e
maxim-08 <alleys_maxim08@icloud.com>
2024-11-19 15:13:40 +0300

    Updated archive

--- archive/routes.rb
+++ archive/routes.rb
@@ -1,1 +1,2 @@
 Algorithm Lambda Functions Firewalls Blockchain Performance Event Sourcing JWT
+GraphQL RabbitMQ Success is not in what you have, but who you are. CI/CD Pull Request Code Review A/B Testing Database Agile Serverless Load Testing JWT Virtual Machines
